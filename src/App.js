287a88a6887a1e9be9eb02bff08564063e3eddf2
Aljobiju <aljovadackel@gmail.com>
2023-03-24 01:34:13 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import Header from './component/Header'
 function App() {
+  const data='COOL'
+  const data1='Aljo'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div>
+      <Header data1={data1}/>
+      <h1 className='hello'>Hello World</h1>   
+  {/* <h1 style={{color:'red'}}>hello world</h1> */}
+  <p>react is so {data}</p>
+  <Demo/>
+  </div>
   );
+}
+
+function Demo(){
+  return(
+    <h2>Easy to use</h2>
+  )
 }
 
 export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+// import { Component } from "react"
+
+// class Header extends Component{
+//   render(){
+//     return(<h1>Hello Aljo {this.props.data}</h1>)
+//   }  
+// }
+
+function Header(props){
+    return(<h1>Hey, This is {props.data1}</h1>)
+}
+export default Header
