c8819da426c33a9384a3bd1de0d1d9e9c76ad240
Aljobiju <aljovadackel@gmail.com>
2023-03-29 22:52:53 +0530

    modifications

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import './App.css'
-import Header from './component/Header'
+import { useState } from "react";
+import Counter from "./component/Counter";
+// import './App.css'
+// import Header from './component/Header'
 function App() {
-  const data='COOL'
-  const data1='Aljo'
-  return (
+  // const data='COOL'
+  // const data1='Aljo'
+  // return (
     
-    <div>
-      <Header data1={data1}/>
-      <h1 className='hello'>Hello World</h1>   
-  {/* <h1 style={{color:'red'}}>hello world</h1> */}
-  <p>react is so {data}</p>
-  <Demo/>
-  </div>
-  );
+  //   <div>
+  //     <Header data1={data1}/>
+  //     <h1 className='hello'>Hello World</h1>   
+  // <h1 style={{color:'red'}}>hello world</h1>
+  // <p>react is so {data}</p>
+  // <Demo/>
+  // </div>
+  // );
+const [count,setCount] =useState(0)
+function addCount(){
+  setCount(count+1)
+}
+let obj={
+  title:'1st',
+  count:count
+}
+return(
+<div className="App">
+  <button onClick={addCount}>Add</button>
+ {/* <Counter title="1st" count={count}/> */}
+ <Counter {...obj}/>
+ <Counter title="2nd" count={count}/>
+</div>
+);
+
 }
 
-function Demo(){
-  return(
-    <h2>Easy to use</h2>
-  )
-}
+// function Demo(){
+//   return(
+//     <h2>Easy to use</h2>
+//   )
+// }
 
 export default App;
